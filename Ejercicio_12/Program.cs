97f4eb5024dc552a2b9d41880db7c94291879f31
RomeroFederico <romerof14@gmail.com>
2016-03-22 17:01:35 -0300

    Main y clase ValidarRespuesta creada.

---
+++ Ejercicio_12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio_12
+{
+    /*
+    12.  Realizar un programa que sume números enteros hasta que el 
+    usuario lo determine, por medio de un mensaje ―¿Continua? (S/N)‖.  
+    En el método estático ValidaS_N() de la clase ValidarRespuesta, 
+    se validará el ingreso de opciones.  
+    El método NO recibe parámetros y devuelve un valor de tipo 
+    booleano, TRUE si se ingreso una 'S' y FALSE si se ingreso una 
+    'N'.  
+    El método deberá validar si otro caracter fue ingresado mostrando 
+    un mensaje de error y pidiendo el reingreso del mismo. 
+    */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro. 12";
+        }
+    }
+}
